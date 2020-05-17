e3d64952cd86c1bafa9dc30a1850341d4b732923
Samuele <samuelepeduzzi@gmail.com>
2020-05-17 20:47:30 +0200

    Delete Negozio

--- ShoppingOnlineWS/src/main/java/resources/StoreResource.java
+++ ShoppingOnlineWS/src/main/java/resources/StoreResource.java
@@ -16,6 +16,7 @@ import java.util.HashMap;
 import java.util.Map;
 import javax.ws.rs.core.*;
 import javax.ws.rs.*;
+import org.apache.commons.codec.digest.DigestUtils;
 import utils.ExtendableBean;
 
 /**
@@ -52,10 +53,13 @@ public class StoreResource {
             Integer idIndirizzo = Integer.parseInt(bean.getProperties().get("IdIndirizzo"));
             Integer idUtente = Integer.parseInt(bean.getProperties().get("IdUtente"));
             
-            if (nome == null || nome.isEmpty() || idIndirizzo == null || idUtente == null)
+            if (nome == null || nome.isEmpty() || idIndirizzo == null || idUtente == null) {
+                System.out.println("params error");
                 throw new WebApplicationException(Response.Status.BAD_REQUEST);
+            }
+                
 
-            String sql = "INSERT INTO negozi (nome, IdUtente, IdIndirizzo) "
+            String sql = "INSERT INTO negozio (nome, CodUtente, CodIndirizzo) "
                     + "VALUES ('" + nome + "', '" + idUtente + "', '" + idIndirizzo + "')";
             
             PreparedStatement stmt = DatabaseConnector.getIstance().getConnection().prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
@@ -76,10 +80,41 @@ public class StoreResource {
             
             
         } catch (SQLException ex) {
+             System.out.println("SQL error");
             throw new WebApplicationException(Response.Status.BAD_REQUEST);  
+           
         } catch (JsonProcessingException ex) {
+            System.out.println("JSON error");
             throw new WebApplicationException(Response.Status.BAD_REQUEST); 
         }
 
+    }
+    @DELETE
+    @Produces(MediaType.APPLICATION_JSON)
+    //@Path("/login")
+    public Response login(@QueryParam("idNegozio")String idNegozio) {
+        try {
+            if (idNegozio == null || idNegozio.isEmpty())
+                throw new WebApplicationException(Response.Status.BAD_REQUEST);  
+            
+            if (!DatabaseConnector.getIstance().isConnected())
+                throw new WebApplicationException("failed to connect to db", 500);
+                
+            PreparedStatement ps = DatabaseConnector.getIstance().getConnection().prepareStatement("DELETE FROM Negozio WHERE ID = " + idNegozio);
+            ResultSet rs = ps.executeQuery();
+            if (!rs.next()) 
+                throw new WebApplicationException(Response.Status.UNAUTHORIZED);          
+            
+            Map<String, String> response = new HashMap();
+            response.put("IdNegozio", String.valueOf(idNegozio));
+            
+            return Response
+                .status(Response.Status.OK)
+                .entity(new ObjectMapper().writeValueAsString(response))
+                .build();
+            
+       } catch (SQLException | JsonProcessingException ex) {
+           throw new WebApplicationException(Response.Status.INTERNAL_SERVER_ERROR); 
+        } 
     }
 }
